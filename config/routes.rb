366fcdfcaf685fb726e2d06864d57318158ebf2f
Sigurd Vive Bergstrom <vive.berg@gmail.com>
2016-03-10 16:49:05 +0100

    Routes of Slip
    af Sigurd

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :devices
   resources :slips
   resources :harbours do
-    resources :piers
+    resources :piers do
+      resources :slips
+    end
   end
-
   
   root 'harbours#index'
 
