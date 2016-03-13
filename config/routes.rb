8ac6684e333d1fd006ed6415c45141ea4423dfdd
Sigurd Vive Bergstrom <vive.berg@gmail.com>
2016-03-13 13:28:39 +0100

    Routes: Devices ind under slips

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   resources :harbours, shallow: true do
     resources :piers do #do skal stadig neste dine routes, da du ellers ikke vil kunne finde de slips der er under en pier
-      resources :slips
+      resources :slips do
+        resources :devices
+      end
     end
-    resources :devices
   end
 
   root 'harbours#index'
