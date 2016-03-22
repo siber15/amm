4098ac0b7f2bc247c2ae6df044dc4548562224d4
Mads Ellesgaard <mads@liveact.dk>
2016-03-22 09:31:31 +0100

    move devices in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   resources :harbours, shallow: true do
     resources :piers do #do skal stadig neste dine routes, da du ellers ikke vil kunne finde de slips der er under en pier
-      resources :slips do
-        resources :devices
-      end
+      resources :slips
     end
   end
+
+  resources :devices
+
 
   root 'harbours#index'
 end
