83c4c24bf80373d63542f84fc993ae53940dfd20
Sigurd Vive Bergstrom <vive.berg@gmail.com>
2016-03-24 22:49:45 +0100

    :question:  2x :bases resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :bases
   resources :harbours, shallow: true do
     resources :piers do #do skal stadig neste dine routes, da du ellers ikke vil kunne finde de slips der er under en pier
       resources :slips
