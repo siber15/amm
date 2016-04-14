50c8ccb7cba0b0acb457d8f31e9b8391cd82dd5b
Sigurd Vive Bergstrom <vive.berg@gmail.com>
2016-04-14 13:38:48 +0200

    Stations routes moved (Not changed)
    Made the overview prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :stations
   resources :harbours, shallow: true do
     resources :piers do #do skal stadig neste dine routes, da du ellers ikke vil kunne finde de slips der er under en pier
       resources :slips
     end
   end
 
+  resources :stations
   resources :devices do
     #route to batch add as a collection
     collection do
